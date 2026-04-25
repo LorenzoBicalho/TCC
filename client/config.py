2818a9da79354b259c39f8a25e168c63fbb53102
LorenzoBicalho <lorenzobicalho@hotmail.com>
2026-04-25 18:31:37 -0300

    if fixo

--- client/config.py
+++ client/config.py
@@ -5,7 +5,7 @@ from utils.hardware import get_cpu_serial
 load_dotenv()
 
 SERIAL_PORT = os.getenv("SERIAL_PORT", "/dev/ttyUSB0") # e.g., 'COM1' for Windows or '/dev/ttyUSB0' for Linux
-SERVER_URL = os.getenv("SERVER_URL", "http://192.168.0.22:8000")
+SERVER_URL = os.getenv("SERVER_URL", "http://192.168.178.50:8000")
 SERIAL_NUMBER = get_cpu_serial()
 NUM_RULES = 5
 NUM_CLUSTERS = 3
